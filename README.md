30e53bfd519ebda5d2f5f613e83d38b2ca997c1a
unknown <you@example.com>
2026-01-21 10:54:00 +0530

    Done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # EnergyConsumption_PredictionModel
+Author Name: Duhita Patil
+ate: 21/01/2026
+
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+from src.data_ingestion import data_ingestion
+
+def main():
+    df = data_ingestion()
+    print(df.head())
+
+
+
+if __name__ == "__main__":
+    main()
+
+def main():
+    print("Hello from energyconsumption-predictionmodel!")
+
